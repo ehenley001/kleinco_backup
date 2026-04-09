3de825cfa9efac4cd625b98a18720b64873039dc
ehenley001 <festivus31@gmail.com>
2026-04-09 11:12:01 -0600

    kleinco-archive-1

---
+++ shoulditeachthisbook/ver2/offline.js
@@ -0,0 +1,18 @@
+﻿{
+	"version": 1512368881,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/tiledbackground2.png",
+		"images/shoulditeachthisbookbg-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
